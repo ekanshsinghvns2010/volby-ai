c90aeae088ec8a7fe7f48c3f5e500ed575f4ac95
!      Ekansh <ekanshsinghvns2010@gmail.com>
2026-08-29 19:34:25 +0530

    Create android/app/src/main/java/com/volbasty/volbyai/MainActivity.java

---
+++ android/app/src/main/java/com/volbasty/volbyai/MainActivity.java
@@ -0,0 +1,44 @@
+package com.volbasty.volbyai;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // Temporary: load the live Volby AI website.
+        webView.loadUrl("https://ekanshsinghvns2010.github.io/volby-ai/");
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
